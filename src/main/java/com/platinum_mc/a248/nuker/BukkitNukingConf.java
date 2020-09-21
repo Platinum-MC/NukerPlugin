63e2d9c5fd656615a24e563816ac36fe63cd231b
A248 <anandebeh@gmail.com>
2020-09-20 20:16:53 -0400

    Increment config version to 4

--- src/main/java/com/platinum_mc/a248/nuker/BukkitNukingConf.java
+++ src/main/java/com/platinum_mc/a248/nuker/BukkitNukingConf.java
@@ -12,7 +12,7 @@ class BukkitNukingConf extends NukingConf {
 	
 	private final Logger logger;
 	
-	private static final int CONFIG_VERSION = 3;
+	private static final int CONFIG_VERSION = 4;
 	
 	BukkitNukingConf(Configuration config) {
 		this.config = config;
